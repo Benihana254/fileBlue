06415da3ff89b4f2831cdb65bdc99a71cf57bdac
Wazakiri <tle439923@Students.mu.ac.ke>
2025-05-11 12:02:43 +0300

    Initial commit for reading and writing files

---
+++ proj1.py
@@ -0,0 +1,9 @@
+import os
+os.makedirs('C:\\delicious\\walnut\\waffles')
+my_files = ['train.docx', 'test.docx', 'blue.txt']
+for file in my_files:
+    result = os.path.join('C:\\Users\\asweigart', file)
+    print(result)
+current_working_directory = os.getcwd()
+print(current_working_directory)
+os.makedirs('C:\\delicious\\walnut\\waffles')
